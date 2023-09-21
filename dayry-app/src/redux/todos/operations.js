3a13be7356173817ccb92c66e4b6d02994272329
Dobrobatko Dmytro <dobrobatka@gmail.com>
2023-09-21 16:54:21 +0300

    add update

--- dayry-app/src/redux/todos/operations.js
+++ dayry-app/src/redux/todos/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https:6454f6daf803f345763642af.mockapi.io";
+axios.defaults.baseURL = "https://6454f6daf803f345763642af.mockapi.io";
 
 export const fetchAllTodos = createAsyncThunk(
   "todos/fetchAll",
